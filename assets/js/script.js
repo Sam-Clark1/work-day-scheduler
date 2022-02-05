7a481785c8c88f32fe58a3f8750db1eb27436709
Sam Clark <samclark2399@gmail.com>
2022-02-04 23:53:12 -0500

    added script to color time blocks based off current time. Can also updated each textarea and it is saved to localstorage

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var today = dayjs().format("dddd, MMMM D")
+ 
+$("#currentDay").append(today);
+ 
+var loadData = function() {
+var id = 0
+for (var i = 9; i <= 17; i++) {
+    if (i < dayjs().hour()) {
+        $(`#${i}`).addClass("past");
+        id++;
+        }
+    else if (i > dayjs().hour()) {
+        $(`#${i}`).addClass("future");
+        id++;
+        }
+    else {
+        $(`#${i}`).addClass("present");
+        id++;
+        }
+    $(`#${i}`).text(localStorage.getItem(id));
+}}
+ 
+$(".saveBtn").on("click", function(){
+    var id = $(this).siblings(".hour").attr("id");
+    var input = $(this).siblings(".description").val().trim();
+    localStorage.setItem(id, input)
+    loadData();
+})
+ 
+loadData();
